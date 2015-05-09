616c23f6f6e75fd8a8038c37e0b6105993bfa58c
leggetter <phil@leggetter.co.uk>
2015-05-09 13:47:34 +0100

    Basics working

--- PusherTest/ViewController.m
+++ PusherTest/ViewController.m
@@ -8,20 +8,74 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <Pusher/Pusher.h>
 
+@interface ViewController ()<PTPusherDelegate> {
+    PTPusher *_client;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"viewDidLoad");
+    
+    // self.client is a strong instance variable of class PTPusher
+    _client = [PTPusher pusherWithKey:@"abb988cdb65953f56d6e" delegate:self encrypted:YES cluster:@"useast2"];
+    
+    // subscribe to channel and bind to event
+    PTPusherChannel *channel = [_client subscribeToChannelNamed:@"chat"];
+    [channel bindToEventNamed:@"new-message" handleWithBlock:^(PTPusherEvent *channelEvent) {
+        // channelEvent.data is a NSDictianary of the JSON object received
+        NSString *message = [channelEvent.data objectForKey:@"text"];
+        NSLog(@"message received: %@", message);
+    }];
+    
+    [_client connect];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//////////////////////////////////
+#pragma mark - Pusher Delegate Connection
+//////////////////////////////////
+
+- (void)pusher:(PTPusher *)pusher connectionDidConnect:(PTPusherConnection *)connection
+{
+    NSLog(@"[Pusher] connected to %@", [connection.URL absoluteString]);
+}
+
+- (void)pusher:(PTPusher *)pusher connection:(PTPusherConnection *)connection failedWithError:(NSError *)error
+{
+    if (error) {
+        NSLog(@"[Pusher] connection failed: %@", [error localizedDescription]);
+    } else {
+        NSLog(@"[Pusher] connection failed");
+    }
+}
+
+- (void)pusher:(PTPusher *)pusher connection:(PTPusherConnection *)connection didDisconnectWithError:(NSError *)error willAttemptReconnect:(BOOL)reconnect
+{
+    if (error) {
+        NSLog(@"[Pusher] didDisconnectWithError: %@ willAttemptReconnect: %@", [error localizedDescription], (reconnect ? @"YES" : @"NO"));
+    } else {
+        NSLog(@"[Pusher] disconnected");
+    }
+}
+
+- (BOOL)pusher:(PTPusher *)pusher connectionWillConnect:(PTPusherConnection *)connection
+{
+    return YES;
+}
+
+- (BOOL)pusher:(PTPusher *)pusher connectionWillAutomaticallyReconnect:(PTPusherConnection *)connection afterDelay:(NSTimeInterval)delay
+{
+    return YES;
 }
 
 @end
